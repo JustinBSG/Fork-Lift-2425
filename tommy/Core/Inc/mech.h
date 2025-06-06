c439325a50a9435d583584f3844e1a55836ac7f4
JustinBSG <justin610810@outlook.com>
2025-06-06 20:41:33 +0800

    fix: tommy: find all unusable gpio and replace them with usable gpio

---
+++ tommy/Core/Inc/mech.h
@@ -0,0 +1,24 @@
+#ifndef __MECH_H__
+#define __MECH_H__
+
+#include <stdbool.h>
+
+#include "main.h"
+
+typedef enum {
+  LINEAR_ACUATOR_UP,
+  LINEAR_ACUATOR_DOWN,
+} LinearActuatorPosition;
+
+typedef struct {
+    int id;
+    LinearActuatorPosition pos;
+} LinearActuator;
+
+void linear_actuator_extend(LinearActuator *actuator);
+
+void linear_actuator_retract(LinearActuator *actuator);
+
+extern LinearActuator linear_actuator[2];
+
+#endif  // __MECH_H__
